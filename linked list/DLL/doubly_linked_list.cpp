20149b406d9b3b6b816aec4923372ff62d6b3bd5
CaptainDevon <divyansh2004ind@gmail.com>
2024-04-07 18:34:32 +0530

    day 3

--- linked list/DLL/doubly_linked_list.cpp
+++ linked list/DLL/doubly_linked_list.cpp
@@ -5,45 +5,87 @@ class node
 public:
     int data;
     node *next;
-    node *back;
+    node *prev;
     node(int value, node *next, node *back)
     {
         this->data = value;
-        this->back = back;
+        this->prev = back;
         this->next = next;
     }
     node(int value)
     {
         this->data = value;
-        this->back = nullptr;
+        this->prev = nullptr;
         this->next = nullptr;
     }
 };
 
-node *array_to_dll(vector<int> nums)
-{
-    node *head = new node(nums[0]);
-    node *prev = head;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        node *temp = new node(nums[i], nullptr, prev);
-        prev->next = temp;
-        prev = temp;
-    }
-    return head;
-}
 
-void print(node* head)
+
+class linkedlist
 {
-    while(head)
-    {
-        cout<<head->data<<" ";
-        head=head->next;
+    private:
+    node* head;
+    public:
+    node * deleteAllOccurrences(node* head, int k) {
+
+        node* temp=head;
+
+        while(temp)
+
+        {   
+
+            if (temp->data == k) 
+
+            {
+
+            if (temp->prev)
+
+                temp->prev->next = temp->next;
+
+            if (temp->next)
+
+                temp->next->prev = temp->prev;
+
+            if (temp == head)
+
+                head = temp->next;
+
+            }
+
+            temp = temp->next;
+
+        }   
+
+        return head;
+
     }
-}
+    node *array_to_dll(vector<int> nums)
+    {
+        node *head = new node(nums[0]);
+        node *prev = head;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            node *temp = new node(nums[i], nullptr, prev);
+            prev->next = temp;
+            prev = temp;
+        }
+        return head;
+    }
+
+    void print(node* head)
+    {
+        while(head)
+        {
+            cout<<head->data<<" ";
+            head=head->next;
+        }
+    }
+};
 int main()
 {
     vector<int> a = {1, 2, 3, 4, 5};
-    node *head = array_to_dll(a);
-    print(head);
+    linkedlist l;
+    node *head = l.array_to_dll(a);
+    l.print(head);
 }
